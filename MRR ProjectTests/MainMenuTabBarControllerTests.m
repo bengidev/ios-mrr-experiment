fafaae59ae7c99bdf7ba66a35fb7c38576e130e9
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-03-23 09:44:16 +0700

    test: cover themed main menu tab bar

--- MRR ProjectTests/MainMenuTabBarControllerTests.m
+++ MRR ProjectTests/MainMenuTabBarControllerTests.m
@@ -205,3 +205,152 @@
 }
 
 @end
+static UIColor *MRRMainMenuTestDynamicFallbackColor(UIColor *lightColor, UIColor *darkColor) {
+  if (@available(iOS 13.0, *)) {
+    return [UIColor colorWithDynamicProvider:^UIColor *(UITraitCollection *traitCollection) {
+      if (traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark) {
+        return darkColor;
+      }
+
+      return lightColor;
+    }];
+  }
+
+  return lightColor;
+}
+
+static UIColor *MRRMainMenuTestNamedColor(NSString *name, UIColor *lightColor, UIColor *darkColor) {
+  UIColor *namedColor = [UIColor colorNamed:name];
+  return namedColor ?: MRRMainMenuTestDynamicFallbackColor(lightColor, darkColor);
+}
+
+static UIColor *MRRMainMenuTestSelectedColor(void) {
+  return MRRMainMenuTestNamedColor(@"AccentColor", [UIColor colorWithRed:0.89 green:0.46 blue:0.24 alpha:1.0],
+                                   [UIColor colorWithRed:0.96 green:0.70 blue:0.47 alpha:1.0]);
+}
+
+static UIColor *MRRMainMenuTestUnselectedColor(void) {
+  return MRRMainMenuTestNamedColor(@"TextSecondaryColor", [UIColor colorWithWhite:0.42 alpha:1.0],
+                                   [UIColor colorWithWhite:0.63 alpha:1.0]);
+}
+
+static UIColor *MRRMainMenuTestBackgroundColor(void) {
+  return MRRMainMenuTestNamedColor(@"CardSurfaceColor", [UIColor colorWithWhite:0.99 alpha:1.0], [UIColor colorWithWhite:0.15 alpha:1.0]);
+}
+
+- (UIColor *)resolvedColor:(UIColor *)color forTraitCollection:(UITraitCollection *)traitCollection;
+- (void)assertColor:(UIColor *)actual matchesColor:(UIColor *)expected;
+- (void)assertItemAppearance:(UITabBarItemAppearance *)itemAppearance
+           selectedIconColor:(UIColor *)selectedIconColor
+             unselectedColor:(UIColor *)unselectedColor API_AVAILABLE(ios(13.0));
+- (void)testMainMenuAppliesThemeColorsToTabBar {
+  UIColor *selectedIconColor = MRRMainMenuTestSelectedColor();
+  UIColor *unselectedColor = MRRMainMenuTestUnselectedColor();
+  UIColor *backgroundColor = MRRMainMenuTestBackgroundColor();
+
+  [self assertColor:self.tabBarController.tabBar.tintColor matchesColor:selectedIconColor];
+  if (@available(iOS 10.0, *)) {
+    [self assertColor:self.tabBarController.tabBar.unselectedItemTintColor matchesColor:unselectedColor];
+  }
+  [self assertColor:self.tabBarController.tabBar.backgroundColor matchesColor:backgroundColor];
+  [self assertColor:self.tabBarController.tabBar.barTintColor matchesColor:backgroundColor];
+  XCTAssertFalse(self.tabBarController.tabBar.translucent);
+
+  if (@available(iOS 13.0, *)) {
+    UITabBarAppearance *appearance = self.tabBarController.tabBar.standardAppearance;
+    XCTAssertNotNil(appearance);
+    [self assertColor:appearance.backgroundColor matchesColor:backgroundColor];
+    [self assertItemAppearance:appearance.stackedLayoutAppearance
+             selectedIconColor:selectedIconColor
+               unselectedColor:unselectedColor];
+    [self assertItemAppearance:appearance.inlineLayoutAppearance
+             selectedIconColor:selectedIconColor
+               unselectedColor:unselectedColor];
+    [self assertItemAppearance:appearance.compactInlineLayoutAppearance
+             selectedIconColor:selectedIconColor
+               unselectedColor:unselectedColor];
+  }
+
+  for (UITabBarItem *item in self.tabBarController.tabBar.items) {
+    XCTAssertEqualWithAccuracy(item.titlePositionAdjustment.horizontal, 0.0, 0.001);
+    XCTAssertEqualWithAccuracy(item.titlePositionAdjustment.vertical, 0.0, 0.001);
+  }
+
+}
+
+- (void)testMainMenuScrollEdgeAppearanceMatchesStandardAppearanceTheme {
+  if (@available(iOS 15.0, *)) {
+    UITabBarAppearance *standardAppearance = self.tabBarController.tabBar.standardAppearance;
+    UITabBarAppearance *scrollEdgeAppearance = self.tabBarController.tabBar.scrollEdgeAppearance;
+
+    XCTAssertNotNil(standardAppearance);
+    XCTAssertNotNil(scrollEdgeAppearance);
+    [self assertColor:scrollEdgeAppearance.backgroundColor matchesColor:standardAppearance.backgroundColor];
+    [self assertItemAppearance:scrollEdgeAppearance.stackedLayoutAppearance
+             selectedIconColor:standardAppearance.stackedLayoutAppearance.selected.iconColor
+               unselectedColor:standardAppearance.stackedLayoutAppearance.normal.iconColor];
+  }
+}
+
+- (UIColor *)resolvedColor:(UIColor *)color forTraitCollection:(UITraitCollection *)traitCollection {
+  if (color == nil) {
+    return nil;
+  }
+
+  if (@available(iOS 13.0, *)) {
+    return [color resolvedColorWithTraitCollection:traitCollection];
+  }
+
+  return color;
+}
+
+- (void)assertColor:(UIColor *)actual matchesColor:(UIColor *)expected {
+  XCTAssertNotNil(actual);
+  XCTAssertNotNil(expected);
+
+  NSArray<UITraitCollection *> *traitCollections = nil;
+  if (@available(iOS 13.0, *)) {
+    traitCollections = @[
+      [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleLight],
+      [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark]
+    ];
+  } else {
+    traitCollections = @[ [UITraitCollection traitCollectionWithTraitsFromCollections:@[]] ];
+  }
+
+  for (UITraitCollection *traitCollection in traitCollections) {
+    UIColor *resolvedActual = [self resolvedColor:actual forTraitCollection:traitCollection];
+    UIColor *resolvedExpected = [self resolvedColor:expected forTraitCollection:traitCollection];
+
+    CGFloat actualRed = 0.0;
+    CGFloat actualGreen = 0.0;
+    CGFloat actualBlue = 0.0;
+    CGFloat actualAlpha = 0.0;
+    CGFloat expectedRed = 0.0;
+    CGFloat expectedGreen = 0.0;
+    CGFloat expectedBlue = 0.0;
+    CGFloat expectedAlpha = 0.0;
+
+    XCTAssertTrue([resolvedActual getRed:&actualRed green:&actualGreen blue:&actualBlue alpha:&actualAlpha]);
+    XCTAssertTrue([resolvedExpected getRed:&expectedRed green:&expectedGreen blue:&expectedBlue alpha:&expectedAlpha]);
+    XCTAssertEqualWithAccuracy(actualRed, expectedRed, 0.001);
+    XCTAssertEqualWithAccuracy(actualGreen, expectedGreen, 0.001);
+    XCTAssertEqualWithAccuracy(actualBlue, expectedBlue, 0.001);
+    XCTAssertEqualWithAccuracy(actualAlpha, expectedAlpha, 0.001);
+  }
+}
+
+- (void)assertItemAppearance:(UITabBarItemAppearance *)itemAppearance
+           selectedIconColor:(UIColor *)selectedIconColor
+             unselectedColor:(UIColor *)unselectedColor API_AVAILABLE(ios(13.0)) {
+  XCTAssertNotNil(itemAppearance);
+  [self assertColor:itemAppearance.selected.iconColor matchesColor:selectedIconColor];
+  [self assertColor:itemAppearance.normal.iconColor matchesColor:unselectedColor];
+  [self assertColor:itemAppearance.selected.titleTextAttributes[NSForegroundColorAttributeName] matchesColor:selectedIconColor];
+  [self assertColor:itemAppearance.normal.titleTextAttributes[NSForegroundColorAttributeName] matchesColor:unselectedColor];
+  XCTAssertEqualWithAccuracy(itemAppearance.selected.titlePositionAdjustment.horizontal, 0.0, 0.001);
+  XCTAssertEqualWithAccuracy(itemAppearance.selected.titlePositionAdjustment.vertical, 0.0, 0.001);
+  XCTAssertEqualWithAccuracy(itemAppearance.normal.titlePositionAdjustment.horizontal, 0.0, 0.001);
+  XCTAssertEqualWithAccuracy(itemAppearance.normal.titlePositionAdjustment.vertical, 0.0, 0.001);
+}
+
