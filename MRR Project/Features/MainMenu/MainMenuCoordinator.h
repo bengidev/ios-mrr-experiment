6d9d2f3a5f30bc459a476d2f0765883ab7a32924
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-03-26 16:05:16 +0700

    Inject saved recipe dependencies into the main menu flow

--- MRR Project/Features/MainMenu/MainMenuCoordinator.h
+++ MRR Project/Features/MainMenu/MainMenuCoordinator.h
@@ -6,11 +6,19 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @class MRRAuthSession;
+@class MRRSavedRecipesStore;
+@protocol MRRLogoutCoordinating;
+@protocol MRRSavedRecipesCloudSyncing;
 
 @interface MainMenuCoordinator : NSObject <MRRFeatureCoordinator>
 
 - (instancetype)initWithAuthenticationController:(id<MRRAuthenticationController>)authenticationController
                                          session:(MRRAuthSession *)session;
+- (instancetype)initWithAuthenticationController:(id<MRRAuthenticationController>)authenticationController
+                                         session:(MRRAuthSession *)session
+                                savedRecipesStore:(nullable MRRSavedRecipesStore *)savedRecipesStore
+                                      syncEngine:(nullable id<MRRSavedRecipesCloudSyncing>)syncEngine
+                                 logoutController:(nullable id<MRRLogoutCoordinating>)logoutController;
 
 @end
 
--- MRR Project/Features/MainMenu/MainMenuCoordinator.m
+++ MRR Project/Features/MainMenu/MainMenuCoordinator.m
@@ -4,12 +4,18 @@
 #import "../Home/HomeCoordinator.h"
 #import "../Profile/ProfileCoordinator.h"
 #import "../Saved/SavedCoordinator.h"
+#import "../../Persistence/SavedRecipes/MRRSavedRecipesStore.h"
+#import "../../Persistence/SavedRecipes/Sync/MRRSavedRecipesCloudSyncing.h"
+#import "../../Persistence/SavedRecipes/Sync/MRRSyncingLogoutController.h"
 #import "MainMenuTabBarController.h"
 
 @interface MainMenuCoordinator ()
 
 @property(nonatomic, retain) id<MRRAuthenticationController> authenticationController;
 @property(nonatomic, retain) MRRAuthSession *session;
+@property(nonatomic, retain, nullable) MRRSavedRecipesStore *savedRecipesStore;
+@property(nonatomic, retain, nullable) id<MRRSavedRecipesCloudSyncing> syncEngine;
+@property(nonatomic, retain, nullable) id<MRRLogoutCoordinating> logoutController;
 @property(nonatomic, retain, nullable) HomeCoordinator *homeCoordinator;
 @property(nonatomic, retain, nullable) SavedCoordinator *savedCoordinator;
 @property(nonatomic, retain, nullable) ProfileCoordinator *profileCoordinator;
@@ -23,6 +29,18 @@
 
 - (instancetype)initWithAuthenticationController:(id<MRRAuthenticationController>)authenticationController
                                          session:(MRRAuthSession *)session {
+  return [self initWithAuthenticationController:authenticationController
+                                        session:session
+                               savedRecipesStore:nil
+                                     syncEngine:nil
+                                logoutController:nil];
+}
+
+- (instancetype)initWithAuthenticationController:(id<MRRAuthenticationController>)authenticationController
+                                         session:(MRRAuthSession *)session
+                                savedRecipesStore:(MRRSavedRecipesStore *)savedRecipesStore
+                                      syncEngine:(id<MRRSavedRecipesCloudSyncing>)syncEngine
+                                 logoutController:(id<MRRLogoutCoordinating>)logoutController {
   NSParameterAssert(authenticationController != nil);
   NSParameterAssert(session != nil);
 
@@ -30,6 +48,9 @@
   if (self) {
     _authenticationController = [authenticationController retain];
     _session = [session retain];
+    _savedRecipesStore = [savedRecipesStore retain];
+    _syncEngine = [syncEngine retain];
+    _logoutController = [logoutController retain];
   }
 
   return self;
@@ -40,6 +61,9 @@
   [_profileCoordinator release];
   [_savedCoordinator release];
   [_homeCoordinator release];
+  [_logoutController release];
+  [_syncEngine release];
+  [_savedRecipesStore release];
   [_session release];
   [_authenticationController release];
   [super dealloc];
@@ -47,10 +71,17 @@
 
 - (UIViewController *)rootViewController {
   if (self.tabBarController == nil) {
-    self.homeCoordinator = [[[HomeCoordinator alloc] initWithSession:self.session] autorelease];
-    self.savedCoordinator = [[[SavedCoordinator alloc] init] autorelease];
+    self.homeCoordinator = [[[HomeCoordinator alloc] initWithSession:self.session
+                                                        dataProvider:nil
+                                                    savedRecipesStore:self.savedRecipesStore
+                                                          syncEngine:self.syncEngine] autorelease];
+    self.savedCoordinator = [[[SavedCoordinator alloc] initWithSessionUserID:self.session.userID
+                                                            savedRecipesStore:self.savedRecipesStore
+                                                                  syncEngine:self.syncEngine] autorelease];
     self.profileCoordinator =
-        [[[ProfileCoordinator alloc] initWithAuthenticationController:self.authenticationController session:self.session] autorelease];
+        [[[ProfileCoordinator alloc] initWithAuthenticationController:self.authenticationController
+                                                             session:self.session
+                                                    logoutController:self.logoutController] autorelease];
 
     NSArray<UIViewController *> *tabViewControllers = @[
       [self navigationControllerForTabFeatureCoordinator:self.homeCoordinator],
