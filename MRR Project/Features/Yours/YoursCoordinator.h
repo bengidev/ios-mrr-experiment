a10f6f4326094d7d814d5a849ed107d0783905c5
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-03-30 14:45:59 +0700

    feat: add yours coordinator

---
+++ MRR Project/Features/Yours/YoursCoordinator.h
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+
+#import "../MainMenu/MRRFeatureCoordinator.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class MRRUserRecipesStore;
+@protocol MRRUserRecipesCloudSyncing;
+
+@interface YoursCoordinator : NSObject <MRRTabFeatureCoordinator>
+
+- (instancetype)init;
+- (instancetype)initWithSessionUserID:(nullable NSString *)sessionUserID
+                      userRecipesStore:(nullable MRRUserRecipesStore *)userRecipesStore
+                            syncEngine:(nullable id<MRRUserRecipesCloudSyncing>)syncEngine;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MRR Project/Features/Yours/YoursCoordinator.m
@@ -0,0 +1,68 @@
+#import "YoursCoordinator.h"
+
+#import "../../Persistence/UserRecipes/MRRUserRecipesStore.h"
+#import "../../Persistence/UserRecipes/Sync/MRRUserRecipesCloudSyncing.h"
+#import "YoursViewController.h"
+
+@interface YoursCoordinator ()
+
+@property(nonatomic, copy, nullable) NSString *sessionUserID;
+@property(nonatomic, retain, nullable) MRRUserRecipesStore *userRecipesStore;
+@property(nonatomic, retain, nullable) id<MRRUserRecipesCloudSyncing> syncEngine;
+@property(nonatomic, retain, nullable) YoursViewController *viewController;
+@property(nonatomic, retain, nullable) UITabBarItem *tabBarItemValue;
+
+@end
+
+@implementation YoursCoordinator
+
+- (instancetype)init {
+  return [self initWithSessionUserID:nil userRecipesStore:nil syncEngine:nil];
+}
+
+- (instancetype)initWithSessionUserID:(NSString *)sessionUserID
+                      userRecipesStore:(MRRUserRecipesStore *)userRecipesStore
+                            syncEngine:(id<MRRUserRecipesCloudSyncing>)syncEngine {
+  self = [super init];
+  if (self) {
+    _sessionUserID = [sessionUserID copy];
+    _userRecipesStore = [userRecipesStore retain];
+    _syncEngine = [syncEngine retain];
+  }
+
+  return self;
+}
+
+- (void)dealloc {
+  [_tabBarItemValue release];
+  [_viewController release];
+  [_syncEngine release];
+  [_userRecipesStore release];
+  [_sessionUserID release];
+  [super dealloc];
+}
+
+- (UIViewController *)rootViewController {
+  if (self.viewController == nil) {
+    self.viewController = [[[YoursViewController alloc] initWithSessionUserID:self.sessionUserID
+                                                             userRecipesStore:self.userRecipesStore
+                                                                   syncEngine:self.syncEngine] autorelease];
+  }
+
+  return self.viewController;
+}
+
+- (UITabBarItem *)tabBarItem {
+  if (self.tabBarItemValue == nil) {
+    UIImage *image = nil;
+    if (@available(iOS 13.0, *)) {
+      image = [UIImage systemImageNamed:@"square.and.pencil"];
+    }
+
+    self.tabBarItemValue = [[[UITabBarItem alloc] initWithTitle:@"Yours" image:image tag:2] autorelease];
+  }
+
+  return self.tabBarItemValue;
+}
+
+@end
