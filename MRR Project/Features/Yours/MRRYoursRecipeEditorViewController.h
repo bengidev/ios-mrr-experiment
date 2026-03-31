c4b8d8b7a2118c10c9f718d69b36285c5ed26538
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-03-31 15:56:44 +0700

    ui: add yours recipe editor interface

---
+++ MRR Project/Features/Yours/MRRYoursRecipeEditorViewController.h
@@ -0,0 +1,20 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class MRRUserRecipeSnapshot;
+@class MRRUserRecipesStore;
+@protocol MRRUserRecipePhotoStorage;
+@protocol MRRUserRecipesCloudSyncing;
+
+@interface MRRYoursRecipeEditorViewController : UIViewController
+
+- (instancetype)initWithSessionUserID:(nullable NSString *)sessionUserID
+                     userRecipesStore:(nullable MRRUserRecipesStore *)userRecipesStore
+                           syncEngine:(nullable id<MRRUserRecipesCloudSyncing>)syncEngine
+                         photoStorage:(nullable id<MRRUserRecipePhotoStorage>)photoStorage
+                        existingRecipe:(nullable MRRUserRecipeSnapshot *)existingRecipe;
+
+@end
+
+NS_ASSUME_NONNULL_END
