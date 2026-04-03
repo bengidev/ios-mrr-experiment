d4be78dd3c817fd8bf6123e639f1ab2286f51f87
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-03 17:37:45 +0700

    Clean step row text field placement

--- MRR Project/Features/Yours/MRRYoursRecipeEditorViewController.m
+++ MRR Project/Features/Yours/MRRYoursRecipeEditorViewController.m
@@ -1178,13 +1178,12 @@ static NSArray<NSString *> *MRRYoursEditorSuggestionTags(void) {
 
     UITextField *textField = [self styledTextFieldWithPlaceholder:[NSString stringWithFormat:@"Step %lu", (unsigned long)(index + 1)] keyboardType:UIKeyboardTypeDefault identifier:[NSString stringWithFormat:@"yours.editor.stepField.%lu", (unsigned long)index]];
     textField.text = resolvedValues[index];
-    [rowView addSubview:textField];
     [textField setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [rowView addSubview:textField];
 
     UIButton *removeButton = [self compactRemoveButtonWithIdentifier:[NSString stringWithFormat:@"yours.editor.removeStepButton.%lu", (unsigned long)index]
                                                   accessibilityLabel:[NSString stringWithFormat:@"Delete step %lu", (unsigned long)(index + 1)]];
     removeButton.tag = (NSInteger)index;
-    removeButton.titleLabel.font = [UIFont systemFontOfSize:14.0 weight:UIFontWeightSemibold];
     [removeButton addTarget:self action:@selector(handleRemoveStepTapped:) forControlEvents:UIControlEventTouchUpInside];
     [rowView addSubview:removeButton];
 
