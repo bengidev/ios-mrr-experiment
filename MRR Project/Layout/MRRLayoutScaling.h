42c85b782f93d81df58f5bed805d706391ba8a35
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-05 18:19:33 +0700

    Apply formatting to Layout files

--- MRR Project/Layout/MRRLayoutScaling.h
+++ MRR Project/Layout/MRRLayoutScaling.h
@@ -10,10 +10,7 @@ typedef NS_ENUM(NSInteger, MRRLayoutScaleAxis) {
 
 FOUNDATION_EXTERN CGFloat MRRLayoutClampedFloat(CGFloat value, CGFloat minimumValue, CGFloat maximumValue);
 FOUNDATION_EXTERN CGFloat MRRLayoutRoundedMetric(CGFloat value);
-FOUNDATION_EXTERN CGFloat MRRLayoutInterpolatedMetricForValue(CGFloat value,
-                                                              CGFloat inputMinimum,
-                                                              CGFloat inputMaximum,
-                                                              CGFloat outputMinimum,
+FOUNDATION_EXTERN CGFloat MRRLayoutInterpolatedMetricForValue(CGFloat value, CGFloat inputMinimum, CGFloat inputMaximum, CGFloat outputMinimum,
                                                               CGFloat outputMaximum);
 FOUNDATION_EXTERN CGFloat MRRLayoutScaleFactor(CGSize viewportSize, MRRLayoutScaleAxis axis);
 FOUNDATION_EXTERN CGFloat MRRLayoutScaledValue(CGFloat baseValue, CGSize viewportSize, MRRLayoutScaleAxis axis);
