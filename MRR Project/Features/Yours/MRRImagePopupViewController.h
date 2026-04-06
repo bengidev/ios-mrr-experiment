a0904870b5c2145553fde97a69541de3f8378ed8
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-06 10:38:24 +0700

    Add MRRImagePopupViewController header

---
+++ MRR Project/Features/Yours/MRRImagePopupViewController.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MRRImagePopupViewController : UIViewController
+
+- (instancetype)initWithImage:(UIImage *)image;
+
+@end
+
+NS_ASSUME_NONNULL_END
